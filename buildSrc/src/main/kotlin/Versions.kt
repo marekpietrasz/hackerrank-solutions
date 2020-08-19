6dd7505db8e03c7fa66daaf6da7d18d1d329001a
marek.pietrasz <marek.pietrasz@gmail.com>
2020-08-19 15:15:14 +0100

    Remove unused versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,8 @@
 object Versions {
     const val junit = "5.6.2"
-    const val jvm = "1.8"
     const val kotlin = "1.3.72"
     const val ktor = "1.2.5"
-    const val kotlinCoroutines = "1.3.2"
     const val ktlint = "0.31.0"
-    const val ktlintPlugin = "9.1.1"
     const val mockk = "1.9.3"
     const val mockito = "1.10.19"
-    const val junitParam = "1.0.4"
-    const val arrow = "0.10.5"
-    const val rxkotlin = "3.0.0"
 }
