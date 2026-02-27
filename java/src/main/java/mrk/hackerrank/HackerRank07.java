5c3b9aaa40c6aeb23d27af615984d54a78106eea
Marek Pietrasz <marek.pietrasz@gmail.com>
2026-02-27 15:51:46 +0000

    Extract DateTimeFormatter constants in HackerRank07

--- java/src/main/java/mrk/hackerrank/HackerRank07.java
+++ java/src/main/java/mrk/hackerrank/HackerRank07.java
@@ -6,9 +6,10 @@ import java.util.Locale;
 
 //https://www.hackerrank.com/challenges/time-conversion/problem
 public class HackerRank07 {
-    public String timeConversion(String s) {
-        LocalTime time = LocalTime.parse(s, DateTimeFormatter.ofPattern("hh:mm:ssa", Locale.US));
-        return time.format(DateTimeFormatter.ofPattern("HH:mm:ss", Locale.US));
-    }
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("hh:mm:ssa", Locale.US);
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss", Locale.US);
 
+    public String timeConversion(String s) {
+        return LocalTime.parse(s, INPUT_FORMATTER).format(OUTPUT_FORMATTER);
+    }
 }
